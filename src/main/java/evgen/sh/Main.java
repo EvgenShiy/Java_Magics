f2e559259d303063a0ad94f6f127d50ce5789189
Evgeniya Shiyanova <evgenshiy@gmail.com>
2025-03-04 23:55:32 +0500

    finished homework 2

--- src/main/java/evgen/sh/Main.java
+++ src/main/java/evgen/sh/Main.java
@@ -6,13 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Oh, you’re a magician, friend! Welcome to Hogwarts!");
 
-        // Первое задание
-        /*
-        В методе main класса Main создайте 5 переменных типа String: dumbledore, mcgonagall, snape, lupin, hugrid.
-        Присвойте каждой переменной значение, соответствующее ее названию. Должны получиться поля, содержащие имена знаменитых волшебников.
-        Затем создайте еще одну строку с названием teachers, содержащую текст: “School teachers:“. Обратите внимание, в тексте содержится двоеточие.
-        Выведите на экран сначала строку teachers, затем все 5 строк с именами преподавателей. Должен получиться список учителей.
-         */
+        // HW1.1
         String dumbledore = "Dumbledore";
         String mcgonagall = "Mcgonagall";
         String snape = "Snape";
@@ -23,20 +17,7 @@ public class Main {
 
         System.out.println(teachers + " " + dumbledore + ", " + mcgonagall + ", " + snape + ", " + lupin + ", " + hugrid);
 
-        // Второе задание
-        /*
-        С помощью new Random().nextInt(1, 10) сгенерируйте 2 разных числа grade1, grade2.
-        Обратите внимание, что класс Random вообще призван, чтобы всегда генерировать случайное число.
-        Тут мы указали конкретный диапазон, потому что работу можно оценить от 1 до 10.
-        Но можно диапазон и не указывать. Каждый раз, когда мы будем вызывать этот метод, будет генерироваться новое число.
-        Теперь давайте проверим, что оба числа больше 5 — это приемлемая для сдачи работы оценка.
-        Используем операции сравнения и &&, чтобы выполнить две проверки на одной строчке.
-        Результат выполнения записываем в переменную типа boolean c названием result. Затем выводим result на экран.
-        Так, если оба числа были больше 5, в результате мы должны получить true.
-        Чтобы проверить, какими были числа, можно перед операцией сравнения тоже вывести их на экран.
-        Затем посчитаем средний балл: сложим две оценки и разделим на два. Подумайте, какой тип данных может получиться в итоге? Результат также вывести на экран.
-         */
-
+        // HW1.2
         int grade1 = new Random().nextInt(1, 10);
         int grade2 = new Random().nextInt(1, 10);
 
@@ -47,8 +28,7 @@ public class Main {
         double average_score = (grade1 + grade2) / 2.0; //Чтобы получить точное значение, нужно использовать double, 2.0 делает деление вещественным
         System.out.println("Средний балл: " + average_score);
 
-        //Третье задание
-        // получить остатки от деления на 13, 14, 15, 16, 17, 19 числа 4199. Затем вычислить сумму остатков от деления
+        //HW1.3
         int i = 4199;
 
         int remainder_13 = i % 13;
@@ -62,5 +42,56 @@ public class Main {
 
         System.out.println("Сумма остатков от деления: " + sum_of_remainders);
 
+
+        //HW2.1
+        String s1 = new Random().nextInt() +
+                String.valueOf(new Random().nextInt()) +
+                new Random().nextInt() +
+                new Random().nextInt() +
+                new Random().nextInt();
+
+        String s2 = s1.replaceAll("1", "i");
+        String s3 = s2.replaceAll("-", "");
+        String s4 = s3.replaceAll("0", "o");
+        String s5 = s4.replaceAll("6", "b");
+
+
+        System.out.println(s5);
+        System.out.println(s5.contains("23"));
+        System.out.println(s5.indexOf("ob"));
+
+        //HW2.2
+        castSpell();
+    }
+
+    public static void castSpell() {
+
+        //исходная строка
+        String s5 = "ARDENTIS VERUM LUMINOS ET FULGUR SYLVESTRA ELIXIA";
+
+        // приведение к нижнему регистру
+        String s6 = s5.toLowerCase();
+
+        //генерация случайного числа
+        int start = new Random().nextInt(50);
+
+        //Создание подстроки от start до конца строки
+        String substring = s6.substring(start);
+
+        //запись в StringBuilder
+        StringBuilder spell = new StringBuilder(substring);
+
+        //разворот строки
+        spell.reverse();
+
+        //Замена символов под 0 и 1 индексами
+        char zero = spell.charAt(0);
+        char one = spell.charAt(1);
+
+        spell.replace(0,1, String.valueOf(one));
+        spell.replace(1,2, String.valueOf(zero));
+
+        //вывод результата
+        System.out.println(spell);
     }
 }
